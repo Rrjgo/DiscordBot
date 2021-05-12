e913c26a31b15d0f5c51f427590d02163268d1f3
Howard Jiang <howardrrjgo@gmail.com>
2021-05-12 17:32:37 +1200

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+
+    if (msg.content.charAt(0) !== '!') {
+        return
+    }
+    const command = msg.content.substring(1).toLowerCase();
+    console.log(msg.content)
+
+    switch (command) {
+        case 'help':
+            console.log('Oranges are $0.59 a pound.');
+            break;
+        case 'commands':
+        case 'ping':
+            msg.reply('pong');
+            break;
+        default:
+            msg.reply('Command not found');
+    }
+});
+
+client.login(process.env.TOKEN);
